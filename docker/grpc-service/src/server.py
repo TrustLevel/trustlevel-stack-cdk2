f8189131f6a5323f74c535e70e4eb2d0fb4911f3
Roman Preuß <romanpre@gmail.com>
2024-06-04 17:37:23 +0200

    Apply formatter on grpc code

--- docker/grpc-service/src/server.py
+++ docker/grpc-service/src/server.py
@@ -3,16 +3,24 @@ import logging
 from concurrent import futures
 import grpc
 import sys
-sys.path.append('/usr/src/app/spec')
+import os
+
+sys.path.append("/usr/src/app/spec")
 
 from spec import trustlevel_pb2
 from spec import trustlevel_pb2_grpc
 
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+logger = logging.getLogger(__name__)
+logger.setLevel(level=LOGLEVEL)
 
 # Assuming calculate_trust_level is in the same directory
-from trust_level_client import calculate_trust_level  # Replace with the correct module name
+from trust_level_client import (
+    calculate_trust_level,
+)  # Replace with the correct module name
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
 
 class TrustlevelService(trustlevel_pb2_grpc.ServiceDefinitionServicer):
     def determineTrustLevel(self, request, context):
@@ -21,25 +29,31 @@ class TrustlevelService(trustlevel_pb2_grpc.ServiceDefinitionServicer):
         # Use the calculate_trust_level function
         try:
             trust_level = calculate_trust_level(input_string)
-            logging.info(f"Calculated trust level: {trust_level} for input: {input_string}")
+            logging.info(
+                f"Calculated trust level: {trust_level} for input: {input_string}"
+            )
             return trustlevel_pb2.Output(trust_level=trust_level)
         except Exception as e:
             logging.error(f"Error in calculate_trust_level: {e}")
             raise grpc.RpcError(grpc.StatusCode.INTERNAL, "Internal server error")
 
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    trustlevel_pb2_grpc.add_ServiceDefinitionServicer_to_server(TrustlevelService(), server)
-    server.add_insecure_port('[::]:7077')
+    trustlevel_pb2_grpc.add_ServiceDefinitionServicer_to_server(
+        TrustlevelService(), server
+    )
+    server.add_insecure_port("[::]:7077")
     server.start()
-    logging.info('Server running on port 7077')
+    logging.info("Server running on port 7077")
     try:
         while True:
             time.sleep(_ONE_DAY_IN_SECONDS)
     except KeyboardInterrupt:
         server.stop(0)
-        logging.info('Server stopped')
+        logging.info("Server stopped")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     serve()
--- docker/grpc-service/src/trust_level_client.py
+++ docker/grpc-service/src/trust_level_client.py
@@ -1,20 +1,31 @@
 import requests
 import json
 import os
+import logging
 
-stage_url = os.environ['STAGE_URL']
-stage_api_key = os.environ['STAGE_API_KEY']
+stage_url = os.environ["STAGE_URL"]
+stage_api_key = os.environ["STAGE_API_KEY"]
+
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+logger = logging.getLogger(__name__)
+logger.setLevel(level=LOGLEVEL)
+
 
 def calculate_trust_level(input_string):
     """Fetches the trust level from a predefined URL."""
+
     url = f"{stage_url}/trustlevels"
+    headers = {"Content-Type": "application/json", "x-api-key": stage_api_key}
+    data = {"text": input_string}
+    logger.debug(f"Sending request to {url} with data: {data}")
 
     try:
         response = requests.post(url, headers=headers, data=json.dumps(data))
         response.raise_for_status()  # Raises an error for bad HTTP status codes
+        logger.debug(f"Received response: {response}")
 
         # Extract the desired field from the JSON response
-        return response.json().get('trustlevel', 0)
+        return response.json().get("trustlevel", 0)
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
         return 0
