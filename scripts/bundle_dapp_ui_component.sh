15be7baafdc8f5747c998c3014f9e3530ed2d58e
Roman Preuß <romanpre@gmail.com>
2024-08-05 08:06:27 +0200

    Add script to bundle dapp component
    [#5]

---
+++ scripts/bundle_dapp_ui_component.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# compress the files in the folder as .zip
+zip -r trustlevel-bias-service.zip ./snet_dapp-master/src/assets/thirdPartyServices/trustlevel_aws_test_id/trustlevel-bias-service/.
